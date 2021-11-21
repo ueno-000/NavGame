b92b9768ba157f2b6ad61e92247a9cf8b171a9ed
uen0_ <sarasa4869@zoho.com>
2021-11-22 06:32:12 +0900

    スキル表示の追加

---
+++ Assets/Sprict/SkillSetumei.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillSetumei : MonoBehaviour
+{
+    [SerializeField] Image Setumei;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // マウスカーソルが対象オブジェクトに重なっている間コールされ続ける
+    void OnMouseOver()
+    {
+        Debug.Log("MouseOver!");
+        Setumei.gameObject.SetActive(true);
+    }
+    // マウスカーソルが対象オブジェクトから退出した時にコールされる
+    void OnMouseExit()
+    {
+        Debug.Log("MouseExit!");
+        Setumei.gameObject.SetActive(false);
+    }
+}
