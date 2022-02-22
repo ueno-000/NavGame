9340aa760c9beea35b42fff4c5c751e33f504025
uen0_ <sarasa4869@zoho.com>
2022-02-22 23:49:35 +0900

    タスクの追加

---
+++ Assets/Sprict/System/Tutorial/Task/HPMPTask.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class HPMPTask : TutorialInterface
+{
+
+    public string _getTitle()
+    {
+        return "HPとMP";
+    }
+
+    public string _getText()
+    {
+        return "下のに2つのバーがあります\r\n上がHP　下がMPです\r\n左クリックで次のタスクへ";
+    }
+
+    public void OnTaskSetting()
+    {
+    }
+
+    public bool _isCheckTask()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public float _transitionTime()
+    {
+        return 2f;
+    }
+}
