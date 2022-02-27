266a50ba91dabc7a4e64805cf7958366ad5a27db
uen0_ <sarasa4869@zoho.com>
2022-02-27 23:49:50 +0900

    Enemyの調整

--- Assets/Sprict/Enemy/EnemyGenerationSprict.cs
+++ Assets/Sprict/Enemy/EnemyGenerationSprict.cs
@@ -11,10 +11,10 @@ public class EnemyGenerationSprict : MonoBehaviour
     [Header("生成ポジションの配列")]
     [SerializeField] Transform[]  _position;
 
+    [Header("SetTImeも同じ値を追加")]
     //生成間隔の時間指定
     [SerializeField] float _time = 5f;
-
-    float _setTime = 5f;
+    [SerializeField] float _setTime = 5f;
     // Update is called once per frame
     void Update()
     {
