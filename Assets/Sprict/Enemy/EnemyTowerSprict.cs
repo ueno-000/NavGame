7748eb6f66d9ed9bb0a3bf45e706729565f73933
uen0_ <sarasa4869@zoho.com>
2022-02-27 23:47:55 +0900

    タワーにHPを表示させた

---
+++ Assets/Sprict/Enemy/EnemyTowerSprict.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTowerSprict : MonoBehaviour
+{
+    [SerializeField,Range(0,100)] int _hp = Mathf.Clamp(100, 0, 100); 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_hp == 0)
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
