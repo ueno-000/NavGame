dec6e030b0288c802c37c76c1b1f4068ae8a5ba5
uen0_ <sarasa4869@zoho.com>
2022-02-27 23:48:43 +0900

    HPのテキストを表示させる用のスクリプト

---
+++ Assets/Sprict/Enemy/TextHPSprict.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class TextHPSprict :HPController
+{
+    [SerializeField] Text _hpText;
+
+    //void Start()
+    //{
+    //    hpSlider = GetComponent<Slider>();
+    //    //スライダーの最大値の設定
+    //    hpSlider.maxValue = maxHp;
+    //}
+
+    /// <summary>Hpをスライダーに表示させるメソッド</summary>
+    public void UpdateSlider(int hp)
+    {
+        hpSlider.value = hp;
+        _hpText.text = hp.ToString();
+    }
+}
