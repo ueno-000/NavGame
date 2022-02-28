9d43922b1f174b90314f43f2c7de5e91b2772f5f
uen0_ <sarasa4869@zoho.com>
2022-03-01 00:11:47 +0900

    タワーのHP処理

--- Assets/Sprict/Enemy/EnemyTowerSprict.cs
+++ Assets/Sprict/Enemy/EnemyTowerSprict.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class EnemyTowerSprict : MonoBehaviour
 {
-    [SerializeField,Range(0,100)] int _hp = Mathf.Clamp(100, 0, 100); 
+    [SerializeField,Range(0,100)] int _hp = Mathf.Clamp(100, 0, 100);
+    [SerializeField] TextHPSprict helth;
 
+    private void Start()
+    {
+        helth = helth.GetComponent<TextHPSprict>();
+    }
     // Update is called once per frame
     void Update()
     {
+        helth.UpdateSlider(_hp);
+
         if(_hp == 0)
         {
+            Debug.Log("タワーが消滅しました");
             Destroy(this.gameObject);
         }
-        
     }
 }
--- Assets/Sprict/Enemy/TextHPSprict.cs
+++ Assets/Sprict/Enemy/TextHPSprict.cs
@@ -4,12 +4,6 @@ public class TextHPSprict :HPController
 {
     [SerializeField] Text _hpText;
 
-    //void Start()
-    //{
-    //    hpSlider = this.GetComponent<Slider>();
-    //    //スライダーの最大値の設定
-    //    hpSlider.maxValue = _maxHp;
-    //}
 
     /// <summary>Hpをスライダーに表示させるメソッド</summary>
     public override void UpdateSlider(int hp)
