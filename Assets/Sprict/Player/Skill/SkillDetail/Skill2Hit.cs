09b4c9257245088408e46ff6743f2f26c68e597f
uen0_ <sarasa4869@zoho.com>
2022-03-02 17:15:54 +0900

    ダメージを送る処理

---
+++ Assets/Sprict/Player/Skill/SkillDetail/Skill2Hit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+/// <summary>
+/// Skill2が与えるダメージ
+/// </summary>
+public class Skill2Hit : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("何かに触れた");
+        // インターフェイスを取得
+        var hit = other.gameObject.GetComponent<IReceiveDamage>();
+        // 触れた相手がダメージを受ける
+        if (hit != null)
+        {
+            hit.ReceiveDamage(10);
+        }
+        //hit.ReceiveDamage(10);
+    }
+}
