87f3af0f9ccf86d95fd746539815eaa9935da8bd
uen0_ <sarasa4869@zoho.com>
2022-03-03 21:48:22 +0900

    Skill1のダメージ処理

---
+++ Assets/Sprict/Player/Skill/SkillDetail/Skill1Hit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+/// <summary>
+/// Skill1が与えるダメージ
+/// </summary>
+public class Skill1Hit : MonoBehaviour
+{
+    private void Start()
+    {
+        Destroy(this.gameObject, 2f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("何かに触れた");
+        // インターフェイスを取得
+        var hit = other.gameObject.GetComponent<IReceiveDamage>();
+        // 触れた相手がダメージを受ける
+        if (hit != null)
+        {
+            hit.ReceiveDamage(10);
+        }
+    }
+}
